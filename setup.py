abe457dda0734d1da2fa73649d3b011c66fb196e
Nitin Verma <nitfver@gmail.com>
2021-05-29 17:00:11 +0200

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project is about identifying the bad wafer from list of all availables wafers.',
+    author='TCS',
+    license='MIT',
+)
